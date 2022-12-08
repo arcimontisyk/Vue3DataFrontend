b72d187d97bfea289eb79d8d4f2554170c7d956b
NH <nils.hoeger@dlr.de>
2022-12-08 16:31:24 +0100

    commandDataField in separate file

--- src/components/payloadCards/CommandCard.vue
+++ src/components/payloadCards/CommandCard.vue
@@ -1,46 +1,4 @@
 
-  <script setup lang="ts">
-//import { mapState2Way } from 'nuxt-socket-io/utils/common';
-import { RouterLink, RouterView } from "vue-router";
-import { ref, onMounted } from "vue";
-import { cardsStore } from "../../stores/cards";
-import { tmtcStore } from "../../stores/tmtc";
-
-const cards = cardsStore();
-const tmtc = tmtcStore();
-const channelName = "default";
-const { testCard } = cards;
-let text = "center";
-
-// text field
-const numberInput = ref(0);
-const textFieldLoading = ref(0);
-
-const props = defineProps({
-  i: Number,
-  content: Object,
-});
-
-const textFieldSubmit = function (event: any) {
-  event.preventDefault();
-  textFieldLoading = false;
-  console.log("event value: " + numberInput);
-};
-
-function changeTextFieldState() {
-  textFieldLoading = false;
-}
-
-const btn_clicked = function (name: string) {
-  console.log("Clicked: " + name);
-};
-const btn_submit = function (name: string) {
-  console.log("Btn submit " + name + " : " + numberInput);
-};
-</script>
-  
-  
-  
   <template>
   <v-card class="mx-auto" max-width="10000">
     <v-card-header>
@@ -54,20 +12,8 @@ const btn_submit = function (name: string) {
             {{ item.fieldName }}
           </v-col>
           <v-col>
-            <v-form>
-              <v-text-field
-                v-model="numberInput"
-                @keydown.enter="textFieldSubmit"
-                required
-                color="success"
-                :loading="textFieldLoading"
-              ></v-text-field>
-            </v-form>
+            <commandValueField :cardItem="item"/>
           </v-col>
-          <!--       <v-col>
-            <v-btn class="mr-4" @click="btn_submit(item.name)"> submit </v-btn>
-          </v-col>
-          -->
         </template>
         <!-- Select command-->
         <template v-if="item.input == 'select'">
@@ -92,3 +38,43 @@ const btn_submit = function (name: string) {
     </v-container>
   </v-card>
 </template>
+    
+        <script setup lang="ts">
+    //import { mapState2Way } from 'nuxt-socket-io/utils/common';
+    import { RouterLink, RouterView } from "vue-router";
+    import { ref, onMounted, watchEffect } from "vue";
+    import { cardsStore } from "../../stores/cards";
+    import { tmtcStore } from "../../stores/tmtc";
+    import commandValueField from "./commandValueField.vue";
+    
+    const cards = cardsStore();
+    const tmtc = tmtcStore();
+    const channelName = "default";
+    const { testCard } = cards;
+    let text = "center";
+    
+    // text field
+    const numberInput = ref(0);
+    const textFieldLoading = ref(false);
+    
+    const props = defineProps({
+      i: Number,
+      content: Object,
+    });
+    
+    const textFieldSubmit = function (event: any) {
+      event.preventDefault();
+      textFieldLoading.value = true;
+      console.log("event value: " + numberInput);
+    };
+    
+    const btn_clicked = function (name: string) {
+      console.log("Clicked: " + name);
+    };
+    
+    watchEffect(() => console.log(numberInput.value));
+    
+    const btn_submit = function (name: string) {
+      console.log("Btn submit " + name + " : " + numberInput);
+    };
+    </script> 
---
+++ src/components/payloadCards/commandValueField.vue
@@ -0,0 +1,51 @@
+    
+<template>
+  <v-form>
+    <v-text-field
+      v-model="numberInput"
+      @keydown.enter="textFieldSubmit"
+      required
+      color="success"
+      :loading="textFieldLoading"
+    ></v-text-field>
+  </v-form>
+</template>
+  
+<script setup lang="ts">
+//import { mapState2Way } from 'nuxt-socket-io/utils/common';
+import { RouterLink, RouterView } from "vue-router";
+import { ref, onMounted, watchEffect } from "vue";
+import { cardsStore } from "../../stores/cards";
+import { tmtcStore } from "../../stores/tmtc";
+
+const cards = cardsStore();
+const tmtc = tmtcStore();
+const channelName = "default";
+let text = "center";
+
+// text field
+const numberInput = ref(0);
+const textFieldLoading = ref(false);
+
+const props = defineProps({
+  cardItem: Object
+});
+
+const textFieldSubmit = function (event: any) {
+  event.preventDefault();
+  textFieldLoading.value = true;
+  console.log("event value: " + numberInput);
+};
+
+const btn_clicked = function (name: string) {
+  console.log("Clicked: " + name);
+};
+
+watchEffect(() => console.log(numberInput.value));
+
+const btn_submit = function (name: string) {
+  console.log("Btn submit " + name + " : " + numberInput);
+};
+</script>
+
+
