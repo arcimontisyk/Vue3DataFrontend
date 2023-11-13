2a25921dbe1be3230f4290b2823b7816f590d287
Nils Höger <github@nils-hoeger.de>
2023-11-13 23:46:27 +0100

    feat(chip): Add chip to cooking card

--- src/components/cooking/recipeCard.vue
+++ src/components/cooking/recipeCard.vue
@@ -1,23 +1,22 @@
 <template>
   <div class="d-flex align-center flex-column">
-    <v-card width="600" title="This is a title" subtitle="This is a subtitle" text="This is content">
+    <v-card width="400" title="This is a title" subtitle="This is a subtitle" text="This is content">
       <template v-slot:text>
-        <v-row>
+        <v-row align="center" justify="center">
           <v-col cols="8">
             <v-row>
-              <v-col cols="3">
-                800
+              <v-col cols="4">
+                800 ml
               </v-col>
-              <v-col cols="3">
-                ml
-              </v-col>
-              <v-col cols="6">
+              <v-col cols="8">
                 Sahne Sahne
               </v-col>
             </v-row>
           </v-col>
           <v-col cols="4">
-            500 kcal
+            <v-chip class="ma-2" color="orange-darken-1" text-color="black">
+              500 kcal
+            </v-chip>
           </v-col>
         </v-row>
       </template></v-card>
