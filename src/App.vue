6f039ae374e581aa88497737b82d484cfe9411ea
Nils Höger <github@nils-hoeger.de>
2023-12-10 22:30:41 +0100

    login menu button with dropdown

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <v-app id="inspire">
     <DynamicLayoutAndOrder>
-     
       <RouterView />
     </DynamicLayoutAndOrder>
   </v-app>
--- src/components/navigation/topNavBar.vue
+++ src/components/navigation/topNavBar.vue
@@ -1,44 +1,48 @@
 <template>
-  <v-app-bar :order="localOrder"  flat title="Application bar">
+  <v-app-bar :order="localOrder" flat title="Application bar">
     <template v-slot:append>
-      <v-switch
-        v-model="localOrder"
-        hide-details
-        inset
-        label="Toggle order"
-        true-value="-1"
-        false-value="0"
-        @change="emitToggleOrder"
-      ></v-switch>
+      <v-switch v-model="localOrder" hide-details inset label="Toggle order" @change="emitToggleOrder"></v-switch>
+      <v-menu offset-y>
+        <template v-slot:activator="{ props }">
+          <v-btn color="primary" v-bind="props" icon>
+            <v-icon>mdi-account-circle</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="(item, index) in dropdownItems" :key="index" :value="index"
+            @click="handleMenuItemClick(item)">
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </template>
   </v-app-bar>
 </template>
 
-<script>
-import { ref, watch } from 'vue';
+<script setup lang="ts">
+import { ref } from 'vue';
 
-export default {
-  props: {
-    order: Number,
-  },
-  setup(props, { emit }) {
-    const localOrder = ref(props.order);
+const dropdownItems = [
+  { title: 'Firebase' },
 
-    watch(
-      () => props.order,
-      (newVal) => {
-        localOrder.value = newVal;
-      }
-    );
+];
 
-    const emitToggleOrder = () => {
-      emit('update:order', localOrder.value);
-    };
+const localOrder = ref(-1);
 
-    return {
-      localOrder,
-      emitToggleOrder,
-    };
-  },
+function handleMenuItemClick(item: any) {
+  // Handle menu item click here
+  console.log(`Clicked on ${item}`);
 };
+
+function emitToggleOrder() {
+  console.log("local order")
+  if (localOrder.value == 0) localOrder.value = -1;
+  else localOrder.value = 0;
+  console.log(localOrder.value)
+}
+
 </script>
+
+<style scoped>
+/* Add your custom styles here */
+</style>
